5df96b760992d265b4cdc8dd6eae12f0acfc3ad2
Cesar <craupesantez@gmail.com>
2020-10-19 23:23:37 -0500

    variables en python

---
+++ variables.py
@@ -0,0 +1,33 @@
+name = "fazt"
+
+name = None
+
+x = 100
+
+book = "Digital fortress"
+
+Bool = "Digita fortress"
+
+#1book =1  no puede definir variable con numeros
+
+print (10+101)
+
+print (name)
+
+x, book = 100, "I Robot"
+
+print(x)
+
+print(x, book)
+
+book_name = "I robot" # Snake case
+book_name =  "the hobbit" #Camel Case
+BookName  = "games of the thrones" #Pascal Case
+
+ PI = 3,1416 # CONVENCION LAS VARIABLES CON MAYUSCULAS SON CONSTANTES
+
+ MY_NAME = "CESAR PESANTEZ"
+
+ book_name = "I robot"
+ book_name = 123234234
+ 
